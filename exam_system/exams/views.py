6dbee27d5c37804ec7759605c43c00acb9797912
wangxinru <849363435@qq.com>
2026-07-02 10:28:22 +0800

    456
    Signed-off-by: wangxinru <849363435@qq.com>

--- exam_system/exams/views.py
+++ exam_system/exams/views.py
@@ -1,4 +1,4 @@
-﻿from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib import messages
 from django.db.models import Count, Avg, Max, Min
